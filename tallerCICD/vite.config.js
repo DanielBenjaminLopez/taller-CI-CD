349b0b00fa81229de29ceb886c602b24e399ba88
benjaminlopez <benja248myn@gmail.com>
2025-05-27 01:09:11 -0300

    Agrega configuraciones

--- tallerCICD/vite.config.js
+++ tallerCICD/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/taller-CI-CD/", // ¡Importante! Nombre exacto de tu repositorio
+  build: {
+    outDir: "dist", // Carpeta de producción (opcional, ya es el valor por defecto)
+    sourcemap: true, // Genera mapas de fuente para debugging (opcional)
+  },
+});
